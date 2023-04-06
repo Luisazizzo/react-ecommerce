736ab0574bf6ed9fe19dc7de3fd00346dc0d7518
Luisazizzo <luisa0zizzo@gmail.com>
2023-04-06 20:33:07 +0200

    06-04-2023

--- src/components/cartItem/CartItem.jsx
+++ src/components/cartItem/CartItem.jsx
@@ -4,10 +4,10 @@ const CartItem = ({ item, deleteCart }) => {
   return (
     <div className="CartItem">
       <img src={item.thumbnail} alt={item.images[0]} />
-      <p>qty:{item.qty}</p>
+      <p>{item.qty}</p>
       <p>{item.title}</p>
       <p>${item.price}</p>
-      <p>Totale:${item.price * item.qty}</p>
+      <p>${item.price * item.qty}</p>
       <p onClick={() => deleteCart(item.id)} className="delete-cart">
         x
       </p>
